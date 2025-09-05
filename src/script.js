71d51a4d4eba498ded55ffaa9ee28da91ea1fad5
hollyjadesmith <hollysmithfachwen@gmail.com>
2025-09-05 21:38:13 +0100

    added function for updating current temperature

--- src/script.js
+++ src/script.js
@@ -1,3 +1,19 @@
+function updateWeather(response){
+    let temperatureElement = document.querySelector("#big-temp");
+    
+    let bigTemp = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(bigTemp);
+}
+
+function searchCity(city){
+// receive the city typed in and search in the API
+// 
+
+let apiKey = "o94ea318dba23fta40204c3af2064fda";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiURL).then(updateWeather)
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
