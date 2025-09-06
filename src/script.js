8827d9f0c338121da072721b7fa39c77ee586b0c
hollyjadesmith <hollysmithfachwen@gmail.com>
2025-09-06 01:18:13 +0100

    updated javascript - temperature element

--- src/script.js
+++ src/script.js
@@ -1,24 +1,25 @@
-function updateWeather(response){
+function updateWeather(response) {
+ 
     let temperatureElement = document.querySelector("#big-temp");
-    
     let bigTemp = response.data.temperature.current;
-    temperatureElement.innerHTML = Math.round(bigTemp);
+    let cityElement = document.querySelector("#city");
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(bigTemp) + "°C";
 }
 
 function searchCity(city){
-// receive the city typed in and search in the API
-// 
 
 let apiKey = "o94ea318dba23fta40204c3af2064fda";
-let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiURL).then(updateWeather)
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
 }
 
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector('#place');
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
+
 }
 
 let searchFormElement = document.querySelector("#search-form");
